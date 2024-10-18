5cfc5d4ba02aeb1f608cfe768b50daf179450e24
dtl06 <tresorleroyd@gmail.com>
2024-10-18 12:31:55 +0100

    Dummy logo added2

--- code/frontend/ride-and-go/app/(pages)/register/page.tsx
+++ code/frontend/ride-and-go/app/(pages)/register/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useLocale } from "@/app/utils/hooks/useLocale.js"; 
+import Link from "next/link";
 
 export default function Register() {
   const { locale } = useLocale();
@@ -64,6 +65,7 @@ export default function Register() {
   };
 
   return (
+    
     <div className="relative min-h-screen bg-cover bg-center" style={{backgroundImage: "url(/images/bg_register.jpeg)"}}>
       <div className="absolute inset-0 bg-black/50 backdrop-blur-sm">
         <div className="flex justify-center items-center min-h-screen">
