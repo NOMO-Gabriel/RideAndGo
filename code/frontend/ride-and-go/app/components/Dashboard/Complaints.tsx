d6f853ad7661d59aec74f76325fc18ee71ba59e5
NOMO-Gabriel <gabriel.nomo@facsciences-uy1.cm>
2024-10-21 04:56:09 +0100

    update complaint component in dashboard

--- code/frontend/ride-and-go/app/components/Dashboard/Complaints.tsx
+++ code/frontend/ride-and-go/app/components/Dashboard/Complaints.tsx
@@ -1,6 +1,9 @@
 'use client';
 import React, { useState } from 'react';
-import { useLocale } from '@/app/utils/hooks/useLocale.js'; // Assurez-vous que ce chemin est correct
+import { useLocale } from '@/app/utils/hooks/useLocale.js';
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus, faTrash, faArchive } from '@fortawesome/free-solid-svg-icons';
 
 // Type pour les plaintes
 interface Complaint {
@@ -22,14 +25,59 @@ const fakeComplaints: Complaint[] = [
     content: 'The service was delayed by 30 minutes. Please investigate this issue.',
     time: '10:00 AM',
     status: 'pending',
-    imageUrl: 'https://via.placeholder.com/50',
+    imageUrl: '/images/profileImageGirl.jpeg',
   },
-  // Ajoutez d'autres plaintes ici...
+  {
+    id: 2,
+    title: 'Billing Issue',
+    miniDescription: 'There was an error in the billing process.',
+    content: 'The billing process had an error. Please check the details.',
+    time: '11:00 AM',
+    status: 'pending',
+    imageUrl: '/images/profileImageGirl.jpeg',
+  },
+  {
+    id: 3,
+    title: 'Support Request',
+    miniDescription: 'Need assistance with a support request.',
+    content: 'Please provide assistance with the support request.',
+    time: '12:00 PM',
+    status: 'resolved',
+    imageUrl: '/images/profileImageGirl.jpeg',
+  },
+  {
+    id: 4,
+    title: 'Service Delay',
+    miniDescription: 'There was a delay in service response.',
+    content: 'The service was delayed by 30 minutes. Please investigate this issue.',
+    time: '1:00 PM',
+    status: 'pending',
+    imageUrl: '/images/profileImageGirl.jpeg',
+  },
+  {
+    id: 5,
+    title: 'Billing Issue',
+    miniDescription: 'There was an error in the billing process.',
+    content: 'The billing process had an error. Please check the details.',
+    time: '2:00 PM',
+    status: 'pending',
+    imageUrl: '/images/profileImageGirl.jpeg',
+  },
+  {
+    id: 6,
+    title: 'Support Request',
+    miniDescription: 'Need assistance with a support request.',
+    content: 'Please provide assistance with the support request.',
+    time: '3:00 PM',
+    status: 'resolved',
+    imageUrl: '/images/profileImageGirl.jpeg',
+  },
 ];
 
 const complaintsContent = {
   en: {
     title: "Complaints",
+    alt: "picture",
     filter: {
       all: "All",
       pending: "Pending",
@@ -41,6 +89,7 @@ const complaintsContent = {
   },
   fr: {
     title: "Plaintes",
+    alt: "photo",
     filter: {
       all: "Tous",
       pending: "En attente",
@@ -55,8 +104,7 @@ const complaintsContent = {
 const Complaints = () => {
   const [filter, setFilter] = useState<'all' | 'pending' | 'resolved'>('all');
   const [complaints, setComplaints] = useState<Complaint[]>(fakeComplaints);
-  const [selectedComplaint, setSelectedComplaint] = useState<Complaint | null>(null);
-  const [showPopup, setShowPopup] = useState<boolean>(false);
+  const [expandedComplaint, setExpandedComplaint] = useState<number | null>(null);
   const { locale } = useLocale(); // Hook d'internationalisation
 
   // Filtrer les plaintes selon le filtre sélectionné
@@ -70,16 +118,9 @@ const Complaints = () => {
     setComplaints(complaints.filter(complaint => complaint.id !== id));
   };
 
-  // Fonction pour afficher le popup
-  const handleShowPopup = (complaint: Complaint) => {
-    setSelectedComplaint(complaint);
-    setShowPopup(true);
-  };
-
-  // Fonction pour fermer le popup
-  const handleClosePopup = () => {
-    setShowPopup(false);
-    setSelectedComplaint(null);
+  // Fonction pour afficher/fermer le contenu déroulant
+  const handleToggleExpand = (id: number) => {
+    setExpandedComplaint(expandedComplaint === id ? null : id);
   };
 
   // Obtenir le contenu localisé
@@ -89,7 +130,7 @@ const Complaints = () => {
     <div className="p-4">
       <h2 className="text-2xl font-bold mb-4">{localizedContent.title}</h2>
       <div className="mb-4">
-        <select value={filter} onChange={(e) => setFilter(e.target.value as 'all' | 'pending' | 'resolved')}>
+        <select className={"p-2 w-max text-center rounded font-medium"} value={filter} onChange={(e) => setFilter(e.target.value as 'all' | 'pending' | 'resolved')}>
           <option value="all">{localizedContent.filter.all}</option>
           <option value="pending">{localizedContent.filter.pending}</option>
           <option value="resolved">{localizedContent.filter.resolved}</option>
@@ -97,54 +138,51 @@ const Complaints = () => {
       </div>
       <div className="flex flex-col space-y-4">
         {filteredComplaints.map(complaint => (
-          <div key={complaint.id} className="bg-gray-200 p-4 rounded-lg shadow-md flex items-center justify-between">
-            <div className="flex items-center space-x-4">
-              <img src={complaint.imageUrl} alt="Complaint" className="w-12 h-12" />
-              <div>
-                <h3 className="font-bold">{complaint.title}</h3>
-                <p className="text-sm">{complaint.miniDescription}</p>
-                <span className="text-xs text-gray-600">{complaint.time}</span>
+          <div key={complaint.id} className="bg-gray-50 p-4 rounded-xl shadow-md border flex flex-col">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center space-x-4">
+                <Image
+                  width={100}
+                  height={100}
+                  alt={localizedContent.alt}
+                  className="w-12 h-12 rounded-full"
+                  src={complaint.imageUrl}
+                />
+                <div>
+                  <h3 className="font-bold">{complaint.title}</h3>
+                  <p className="text-sm">{complaint.miniDescription}</p>
+                  <span className="text-xs text-blue-600">{complaint.time}</span>
+                </div>
+              </div>
+              <div className="flex space-x-2">
+                <button
+                  className="text-blue-600"
+                  onClick={() => handleToggleExpand(complaint.id)}
+                >
+                  <FontAwesomeIcon icon={expandedComplaint === complaint.id ? faMinus : faPlus} />
+                </button>
+                <button
+                  className="text-red-600"
+                  onClick={() => handleDelete(complaint.id)}
+                >
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
+                <button
+                  className="text-green-600"
+                  onClick={() => console.log(`Archiving complaint with id: ${complaint.id}`)}
+                >
+                  <FontAwesomeIcon icon={faArchive} />
+                </button>
               </div>
             </div>
-            <div className="flex space-x-2">
-              <button
-                className="text-blue-600"
-                onClick={() => handleShowPopup(complaint)}
-              >
-                Voir plus
-              </button>
-              <button
-                className="text-red-600"
-                onClick={() => handleDelete(complaint.id)}
-              >
-                Supprimer
-              </button>
-              <button
-                className="text-green-600"
-                onClick={() => console.log(`Archiving complaint with id: ${complaint.id}`)}
-              >
-                Archiver
-              </button>
-            </div>
+            {expandedComplaint === complaint.id && (
+              <div className="mt-4 bg-white p-4 rounded shadow-lg">
+                <p className='text-start text-base'>{complaint.content}</p>
+              </div>
+            )}
           </div>
         ))}
       </div>
-
-      {/* Popup pour afficher le contenu complet de la plainte */}
-      {showPopup && selectedComplaint && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <div className="bg-white p-4 rounded shadow-lg w-96">
-            <h3 className="text-xl font-bold mb-2">{selectedComplaint.title}</h3>
-            <p>{selectedComplaint.content}</p>
-            <button
-              className="mt-4 bg-blue-600 text-white px-4 py-2 rounded"
-              onClick={handleClosePopup}
-            >
-              {localizedContent.popup.close}
-            </button>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
