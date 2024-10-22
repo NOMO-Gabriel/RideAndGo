0c857a08078212506d5092b4099f5fe7000f9365
NOMO-Gabriel <gabriel.nomo@facsciences-uy1.cm>
2024-10-22 23:34:34 +0100

    backend project created

---
+++ code/backend/rideAndGo/src/test/java/com/rideAndGo/rideAndGo/RideAndGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rideAndGo.rideAndGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideAndGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
