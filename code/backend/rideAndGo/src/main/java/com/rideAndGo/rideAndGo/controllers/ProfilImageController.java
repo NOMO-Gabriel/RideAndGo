e45eefb094b4f75d612723773fd6e83e99896fec
estelleNdongo <estellendongo02@gmail.com>
2024-11-11 12:02:49 +0100

    test sur la gestion des pdf

--- code/backend/rideAndGo/src/main/java/com/rideAndGo/rideAndGo/controllers/ProfilImageController.java
+++ code/backend/rideAndGo/src/main/java/com/rideAndGo/rideAndGo/controllers/ProfilImageController.java
@@ -1,4 +1,6 @@
 package com.rideAndGo.rideAndGo.controllers;
+import com.rideAndGo.rideAndGo.services.ProfilImageService;
+
 
 import java.io.IOException;
 import java.util.UUID;
@@ -7,10 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 import com.rideAndGo.rideAndGo.models.ProfilImages;
-import com.rideAndGo.rideAndGo.services.ProfilImageService;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.core.io.Resource;
+
+
 
 
 @RestController
@@ -34,6 +41,43 @@ public class ProfilImageController {
        
         
     }
+
+    @PutMapping("/{imageId}")
+    public ResponseEntity <ProfilImages> updateProfileImage(@PathVariable UUID imageId, @RequestParam("file") MultipartFile newFile) {
+        try{
+            ProfilImages updatedImage = profilImageService.updateProfilImages(imageId, newFile);
+            return ResponseEntity.ok(updatedImage);
+        }catch(IOException e){
+            e.printStackTrace();
+            return ResponseEntity.status(500).build();
+        }
+    
+    }
+
+    @DeleteMapping("/{imageId}")
+    public ResponseEntity <Void> deleteProfilImege(@PathVariable UUID imageId){
+        try{
+            profilImageService.deleteProfileImage(imageId);
+            return ResponseEntity.noContent().build();
+
+        }catch (IOException e){
+            e.printStackTrace();
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+    @GetMapping("/{imageId}")
+    public ResponseEntity <Resource> getProfileImageById (@PathVariable UUID imageId) {
+        try{
+            return profilImageService.getProfilImageById(imageId);
+
+        }catch(IOException e){
+            e.printStackTrace();
+            return ResponseEntity.status(404).build();
+        }
+        
+    }
+    
     
 
 
